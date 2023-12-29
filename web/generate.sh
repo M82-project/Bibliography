a9a48429973315d5bc6fdefc460165182fd5c8af
Sebastien Larinier <sebdraven@protonmail.com>
2023-12-29 12:33:51 +0100

    All bib test (#52)
    * update
    
    * update
    
    * test
    
    * Update Septembre_2023.bib
    
    * last test
    
    * update
    
    * update build
    
    * update
    
    * update
    
    * Update docker-image.yml
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update docker-image.yml
    
    * update
    
    * Update Dockerfile
    
    * update
    
    * update
    
    * Update check.sh
    
    * Update docker-image.yml

--- web/generate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docs=("Juillet_2023" "Novembre_2023" "Septembre_2023")
-for doc in ${docs[@]}; do
-        pandoc -t markdown_strict --citeproc /data/web/template.md -o ${doc}/${doc}.md --bibliography ${doc}/*.bib 
-done
